76e4a5401932e68558a7db3273232ce114d21801
孟松 <mengsong@58.com>
2023-08-14 17:32:44 +0800

    cglib动态代理

--- think/src/main/java/thinking/proxy/cglibdemo/Main.java
+++ think/src/main/java/thinking/proxy/cglibdemo/Main.java
@@ -1,4 +1,15 @@
 package thinking.proxy.cglibdemo;
 
 public class Main {
+    public static void main(String[] args) {
+        //TVFactoryA
+        TVFactoryA tvFactoryA = new TVProxy<TVFactoryA>().getProxy(TVFactoryA.class);
+        System.out.println(tvFactoryA.produceTV());
+        tvFactoryA.repairTV();
+        System.out.println("------------------------------------------------------------------------------------");
+        //TVFactoryB
+        TVFactoryB tvFactoryB = new TVProxy<TVFactoryB>().getProxy(TVFactoryB.class);
+        System.out.println(tvFactoryB.produceTV());
+        tvFactoryB.repairTV();
+    }
 }
--- think/src/main/java/thinking/proxy/cglibdemo/TVProxy.java
+++ think/src/main/java/thinking/proxy/cglibdemo/TVProxy.java
@@ -1,3 +1,38 @@
 package thinking.proxy.cglibdemo;
-public class TVProxy{
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class TVProxy<T> implements MethodInterceptor {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public T getProxy(Class<T> tClass) {
+        this.setName("【"+tClass.getSimpleName()+"-PROXY】");
+        //1.工具类
+        Enhancer enhancer = new Enhancer();
+        //2.设置父类
+        enhancer.setSuperclass(tClass);
+        //3.设置回调函数
+        enhancer.setCallback(this);
+        //4.创建子类（代理对象）
+        return (T)enhancer.create();
+    }
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+        System.out.println(name+"before");
+        Object invoke = proxy.invokeSuper(obj, args);
+        System.out.println(name+"after");
+        return invoke;
+    }
 }
