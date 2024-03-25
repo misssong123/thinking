573f5c898ec212a1cc25b6c557215e5e2d3e4b0b
孟松 <mengsong@58.com>
2024-03-25 19:42:37 +0800

    动态规划-不同路径1

---
+++ algorithm-template/src/main/java/com/meng/specialSubject/knapsack/diffRoute/Q001UniquePaths.java
@@ -0,0 +1,41 @@
+package com.meng.specialSubject.knapsack.diffRoute;
+
+/**
+ * 62. 不同路径
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * 问总共有多少条不同的路径？
+ */
+public class Q001UniquePaths {
+    /**
+     * 0
+     * ms
+     * 击败
+     * 100.00%
+     * 使用 Java 的用户
+     * 消耗内存分布
+     * 39.26
+     * MB
+     * 击败
+     * 87.42%
+     * 使用 Java 的用户
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        int [][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++){
+            dp[0][i] = 1;
+        }
+        for(int i= 1; i < m; i++){
+            for(int j = 1 ; j < n ; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return  dp[m-1][n-1];
+    }
+}
