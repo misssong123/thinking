2e1dea25fc9e106b8c95ae77fc8c08c42e5bc5e8
孟松 <mengsong@58.com>
2024-03-27 11:28:48 +0800

    动态规划-最小路径和问题(数组)多个终点-下左右移动

--- algorithm-template/src/main/java/com/meng/specialSubject/knapsack/diffRoute/Q003MinPathSum.java
+++ algorithm-template/src/main/java/com/meng/specialSubject/knapsack/diffRoute/Q003MinPathSum.java
@@ -2,6 +2,11 @@ package com.meng.specialSubject.knapsack.diffRoute;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ 这是 LeetCode 上的「64. 最小路径和」，难度为 Medium。
+ 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ 说明：每次只能向下或者向右移动一步。
+ */
 public class Q003MinPathSum {
     /**
      * 2
