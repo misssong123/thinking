2a1315ca6bb6df6e1377fb0432dbb93989481fdf
孟松 <mengsong@58.com>
2024-04-09 16:21:53 +0800

    使用二分法查询指定元素的不同场景

--- algorithm/src/main/java/com/meng/specialSubject/search/binary_search/BinarySearch01.java
+++ algorithm/src/main/java/com/meng/specialSubject/search/binary_search/BinarySearch01.java
@@ -1,28 +1,94 @@
 package com.meng.specialSubject.search.binary_search;
 
 /**
- * 从指定升序数组中查找指定元素,不存在返回-1，反之返回对应下标
+ * 从非降序数组中查找指定元素
  */
 public class BinarySearch01 {
     public static void main(String[] args) {
-        int[]  nums = {1,2,3,4,5,6,7,8,9};
-        System.out.println(binarySearch(nums,10));
+        int[] nums =  {1,1,1,2,2,2,2,2,2,2,2,2,2,2,4,5,6,8,9,10};
+        int target = 7;
+        System.out.println(targetValFirstIndex(nums,target));
+        System.out.println(targetValEndIndex(nums,target));
+        System.out.println(lowerBound(nums,target));
+        System.out.println(highBound(nums,target));
     }
-    public static int binarySearch(int[] nums, int target) {
-        if (nums==null || nums.length==0){
-            return -1;
-        }
-        int left = 0 ,right =  nums.length-1;
-        while (left <= right){
-            int mid = left + (right-left)/2;
-            if (nums[mid] == target){
-                return mid;
-            }else if (nums[mid] < target){
-                left = mid+1;
+
+    /**
+     * 指定元素的第一个位置,不存在返回-1
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int targetValFirstIndex(int[] nums, int val) {
+        int left = 0, right = nums.length-1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left)/ 2;
+            if (nums[mid]>= val){
+                ans = nums[mid] == val?mid:ans;
+                right = mid - 1;
             }else {
-                right = mid-1;
+                left = mid + 1;
             }
         }
-        return -1;
+        return ans;
+    }
+    /**
+     * 指定元素的最后一个位置,不存在返回-1
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int targetValEndIndex(int[] nums, int val) {
+        int left = 0, right = nums.length-1;
+        int ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left)/ 2;
+            if (nums[mid] <= val){
+                ans = nums[mid] == val?mid:ans;
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 小于指定元素的第一个位置,不存在返回-1
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int lowerBound(int[] nums, int val) {
+        int left = 0, right = nums.length-1;
+        while (left <= right) {
+            int mid = left + (right - left)/ 2;
+            if (nums[mid]>=val){
+                right = mid - 1;
+            }else {
+                left = mid+1;
+            }
+        }
+        return right;
+    }
+
+    /**
+     *  大于等于指定元素的第一个位置,不存在返回-1
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int highBound(int[] nums, int val) {
+        int left = 0, right = nums.length-1;
+        while (left <= right) {
+            int mid = left + (right - left)/ 2;
+            if (nums[mid]>val){
+                right = mid - 1;
+            }else {
+                left = mid+1;
+            }
+        }
+        return left==nums.length?-1:left;
     }
 }
