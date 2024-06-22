d2a566546c97af2771e9e42dc7541ad2ff6ba345
孟松 <mengsong@58.com>
2024-06-22 16:55:51 +0800

    树状数组

--- algorithm/src/main/java/com/meng/specialSubject/array/treearray/TreeArrayTemplate.java
+++ algorithm/src/main/java/com/meng/specialSubject/array/treearray/TreeArrayTemplate.java
@@ -6,12 +6,6 @@ public class TreeArrayTemplate {
     public static void main(String[] args) {
         int[] nums = {1,3,5,2,4,7};
         NumArray demo = new NumArray(nums);
-        for (int i = 1 ; i <= 6 ; i++){
-            for (int j = i; j <= 5; j += demo.lowbit(j)) {
-                System.out.println(j);
-            }
-            System.out.println("------------------------");
-        }
         demo.update(2,10);
         System.out.println(demo.sumRange(0,2));
         System.out.println(demo.sumRange(3,4));
@@ -24,18 +18,18 @@ public class TreeArrayTemplate {
 
 class NumArray {
     int[] tree;
-    int lowbit(int x) {
+    int lowBit(int x) {
         return x & -x;
     }
     int query(int x) {
         int ans = 0;
-        for (int i = x; i > 0; i -= lowbit(i)) {
+        for (int i = x; i > 0; i -= lowBit(i)) {
             ans += tree[i];
         }
         return ans;
     }
     void add(int x, int u) {
-        for (int i = x; i <= n; i += lowbit(i)) {
+        for (int i = x; i <= n; i += lowBit(i)) {
             tree[i] += u;
         }
     }
@@ -61,40 +55,3 @@ class NumArray {
         return query(r + 1) - query(l);
     }
 }
-/*
-// 上来先把三个方法写出来
-{
-        int[] tree;
-        int lowbit(int x) {
-        return x & -x;
-        }
-        // 查询前缀和的方法
-        int query(int x) {
-        int ans = 0;
-        for (int i = x; i > 0; i -= lowbit(i)) ans += tree[i];
-        return ans;
-        }
-        // 在树状数组 x 位置中增加值 u
-        void add(int x, int u) {
-        for (int i = x; i <= n; i += lowbit(i)) tree[i] += u;
-        }
-        }
-
-// 初始化「树状数组」，要默认数组是从 1 开始
-        {
-        for (int i = 0; i < n; i++) add(i + 1, nums[i]);
-        }
-
-// 使用「树状数组」：
-        {
-        void update(int i, int val) {
-        // 原有的值是 nums[i]，要使得修改为 val，需要增加 val - nums[i]
-        add(i + 1, val - nums[i]);
-        nums[i] = val;
-        }
-
-        int sumRange(int l, int r) {
-        return query(r + 1) - query(l);
-        }
-        }
-        */
