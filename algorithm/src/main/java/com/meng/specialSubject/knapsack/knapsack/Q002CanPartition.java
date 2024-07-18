1971b8d90643054327229ebe81f1c9c236a2a34e
孟松 <mengsong@58.com>
2024-07-18 17:07:12 +0800

    背包问题(具体案例应用)

---
+++ algorithm/src/main/java/com/meng/specialSubject/knapsack/knapsack/Q002CanPartition.java
@@ -0,0 +1,41 @@
+package com.meng.specialSubject.knapsack.knapsack;
+
+import java.util.Arrays;
+
+public class Q002CanPartition {
+    /**
+     * 执行用时分布
+     * 22
+     * ms
+     * 击败
+     * 76.72%
+     * 复杂度分析
+     * 消耗内存分布
+     * 41.43
+     * MB
+     * 击败
+     * 53.72%
+     * @param nums
+     * @return
+     */
+    public boolean canPartition(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        if (sum% 2 != 0){
+            return false;
+        }
+        int max = sum /2 ;
+        int[] cache = new int[max + 1];
+        for(int num : nums){
+            if (num > max){
+                return false;
+            }
+            if (num == max){
+                return true;
+            }
+            for (int j = max; j >= num; j--){
+                cache[j] = Math.max(cache[j], cache[j - num] + num);
+            }
+        }
+        return cache[max] == max;
+    }
+}
