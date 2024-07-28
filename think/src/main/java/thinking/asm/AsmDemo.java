26638b59ec7791c24a74635070efc1703fcf738e
孟松 <mengsong@58.com>
2024-07-28 18:54:30 +0800

    asm简单说明

--- think/src/main/java/thinking/asm/AsmDemo.java
+++ think/src/main/java/thinking/asm/AsmDemo.java
@@ -1,7 +1,22 @@
 package thinking.asm;
 
+import thinking.asm.generator.HelloWorldGenerator;
+
 public class AsmDemo {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        try {
+            byte[] bytes = HelloWorldGenerator.generateHelloWorldClass("HelloWorld");
+            CustomerClassLoader loader = new CustomerClassLoader();
+            Class<?> helloWorldClass = loader.defineClass("HelloWorld", bytes);
+            helloWorldClass.getMethod("main",String[].class).invoke(null,(Object)args);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    public static class CustomerClassLoader extends ClassLoader {
+        public Class<?> defineClass(String name, byte[] b) {
+            return super.defineClass(name, b, 0, b.length);
+        }
     }
 }
--- think/src/main/java/thinking/asm/generator/HelloWorldGenerator.java
+++ think/src/main/java/thinking/asm/generator/HelloWorldGenerator.java
@@ -1,10 +1,32 @@
 package thinking.asm.generator;
 
 import jdk.internal.org.objectweb.asm.ClassWriter;
+import jdk.internal.org.objectweb.asm.MethodVisitor;
+import jdk.internal.org.objectweb.asm.Opcodes;
 
 public class HelloWorldGenerator {
     public static byte[] generateHelloWorldClass(String className){
         ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-        return null;
+        //定义
+        cw.visit(Opcodes.V1_8,Opcodes.ACC_PUBLIC,className,null,"java/lang/Object",null);
+        //init方法
+        MethodVisitor initMethod = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
+        initMethod.visitCode();
+        initMethod.visitVarInsn(Opcodes.ALOAD,0);
+        initMethod.visitMethodInsn(Opcodes.INVOKESPECIAL,"java/lang/Object","<init>","()V",false);
+        initMethod.visitInsn(Opcodes.RETURN);
+        initMethod.visitMaxs(1,1);
+        initMethod.visitEnd();
+        //main方法
+        MethodVisitor mainMethod = cw.visitMethod(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+        mainMethod.visitCode();
+        mainMethod.visitFieldInsn(Opcodes.GETSTATIC,"java/lang/System","out","Ljava/io/PrintStream;");
+        mainMethod.visitLdcInsn("Hello, world!");
+        mainMethod.visitMethodInsn(Opcodes.INVOKEVIRTUAL,"java/io/PrintStream","println","(Ljava/lang/String;)V",false);
+        mainMethod.visitInsn(Opcodes.RETURN);
+        mainMethod.visitMaxs(2,2);
+        mainMethod.visitEnd();
+        cw.visitEnd();
+        return cw.toByteArray();
     }
 }
