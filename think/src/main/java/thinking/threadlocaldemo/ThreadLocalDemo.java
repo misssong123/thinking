c60613f0c64972331a6aa51650da40d8268ad0e4
孟松 <mengsong@58.com>
2024-08-28 19:11:28 +0800

    ThreadLocal样例

--- think/src/main/java/thinking/threadlocaldemo/ThreadLocalDemo.java
+++ think/src/main/java/thinking/threadlocaldemo/ThreadLocalDemo.java
@@ -4,13 +4,22 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class ThreadLocalDemo {
-    static ThreadLocal<Integer> NUM = ThreadLocal.withInitial(()->0);
+    /**
+     * 声明方式
+     * 1.ThreadLocal.withInitial(()->0) 可以进行初始化，实现类为SuppliedThreadLocal
+     * 2. new ThreadLocal<>();不可以进行初始化
+     */
+    //static ThreadLocal<Integer> NUM = ThreadLocal.withInitial(()->0);
+    static ThreadLocal<Integer> NUM = new ThreadLocal<>();
     public static void main(String[] args) {
         ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(2,2,10L,
                 TimeUnit.SECONDS,new ArrayBlockingQueue<>(3000));
-        NUM.set(1);
+        //NUM.set(1);
         for(int i = 0 ; i < 10 ; i++){
             poolExecutor.execute(() -> {
+                if (NUM.get() == null){
+                    NUM.set(0);
+                }
                 NUM.set(NUM.get()+1);
                 try {
                     TimeUnit.SECONDS.sleep(2);
