a5fb6b89acaee87ea0dca8a047fa88b9779b5279
孟松 <mengsong@58.com>
2024-09-02 14:25:11 +0800

    InheritableThreadLocal的使用

--- think/src/main/java/thinking/threadlocaldemo/InheritableThreadLocalDemo.java
+++ think/src/main/java/thinking/threadlocaldemo/InheritableThreadLocalDemo.java
@@ -5,55 +5,51 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class InheritableThreadLocalDemo {
-    static ThreadLocal<Integer> NUM = new InheritableThreadLocal<>();
-    static ThreadLocal<Integer> NUM1 = InheritableThreadLocal.withInitial(() -> 0);
+    static InheritableThreadLocal<Integer> NUM = new InheritableThreadLocal<>();
+    public static void test1(){
+        Thread thread1 = new Thread(()->{
+            NUM.set(NUM.get()+1);
+            System.out.println("线程:"+Thread.currentThread().getName()+";NUM:"+NUM.get());
+            Thread thread3 = new Thread(()->{
+                NUM.set(NUM.get()+1);
+                System.out.println("线程:"+Thread.currentThread().getName()+";NUM:"+NUM.get());
+                //移除
+                NUM.remove();
+            },"thread3");
+            thread3.start();
+            //移除
+            NUM.remove();
+        },"thread1");
+        thread1.start();
+        Thread thread2 = new Thread(()->{
+            NUM.set(NUM.get()+1);
+            System.out.println("线程:"+Thread.currentThread().getName()+";NUM:"+NUM.get());
+            //移除
+            NUM.remove();
+        },"thread2");
+        thread2.start();
+    }
     public static void main(String[] args) {
         NUM.set(1);
-        NUM1.set(1);
-//        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(2,2,10L,
-//                TimeUnit.SECONDS,new ArrayBlockingQueue<>(3000));
-//        for(int i = 0 ; i < 10 ; i++){
-//            poolExecutor.execute(() -> {
-//                NUM.set(NUM.get()+1);
-//                try {
-//                    TimeUnit.SECONDS.sleep(2);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                System.out.println("线程:"+Thread.currentThread().getName()+";NUM:"+NUM.get());
-//                //移除
-//                NUM.remove();
-//            });
-//        }
-//        poolExecutor.shutdown();
-        new Thread(()->{
-            System.out.println("线程:"+Thread.currentThread().getName()+";NUM:"+NUM.get()+";NUM1:"+NUM1.get());
-            NUM.set(NUM.get()+1);
-            NUM1.set(NUM1.get()+1);
-            try {
-                TimeUnit.SECONDS.sleep(2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("线程:"+Thread.currentThread().getName()+";NUM:"+NUM.get()+";NUM1:"+NUM1.get());
-            //移除
-            NUM.remove();
-            NUM1.remove();
-        }).start();
-        new Thread(()->{
-            System.out.println("线程:"+Thread.currentThread().getName()+";NUM:"+NUM.get()+";NUM1:"+NUM1.get());
-            NUM.set(NUM.get()+1);
-            NUM1.set(NUM1.get()+1);
-            try {
-                TimeUnit.SECONDS.sleep(2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("线程:"+Thread.currentThread().getName()+";NUM:"+NUM.get()+";NUM1:"+NUM1.get());
-            //移除
-            NUM.remove();
-            NUM1.remove();
-        }).start();
+        test1();
+    }
+    public static void test2(){
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(2,2,10L,
+                TimeUnit.SECONDS,new ArrayBlockingQueue<>(3000));
+        for(int i = 0 ; i < 10 ; i++){
+            poolExecutor.execute(() -> {
+                NUM.set(NUM.get()+1);
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("线程:"+Thread.currentThread().getName()+";NUM:"+NUM.get());
+                //移除
+                //NUM.remove();
+            });
+        }
+        poolExecutor.shutdown();
     }
 
 }
