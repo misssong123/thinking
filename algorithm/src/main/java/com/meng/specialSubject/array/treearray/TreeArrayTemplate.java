4aa2818bd7c099db0736f08a835dd027c1fad3fe
孟松 <mengsong@58.com>
2025-04-15 15:40:54 +0800

    补充树状数组代码

--- algorithm/src/main/java/com/meng/specialSubject/array/treearray/TreeArrayTemplate.java
+++ algorithm/src/main/java/com/meng/specialSubject/array/treearray/TreeArrayTemplate.java
@@ -55,3 +55,74 @@ class NumArray {
         return query(r + 1) - query(l);
     }
 }
+class NumArray01 {
+    private int[] nums;
+    private int[] tree;
+
+    public NumArray01(int[] nums) {
+        int n = nums.length;
+        this.nums = new int[n]; // 使 update 中算出的 delta = nums[i]
+        tree = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            update(i, nums[i]);
+        }
+    }
+
+    public void update(int index, int val) {
+        int delta = val - nums[index];
+        nums[index] = val;
+        for (int i = index + 1; i < tree.length; i += i & -i) {
+            tree[i] += delta;
+        }
+    }
+
+    private int prefixSum(int i) {
+        int s = 0;
+        for (; i > 0; i &= i - 1) { // i -= i & -i 的另一种写法
+            s += tree[i];
+        }
+        return s;
+    }
+
+    public int sumRange(int left, int right) {
+        return prefixSum(right + 1) - prefixSum(left);
+    }
+}
+
+class NumArray02 {
+    private int[] nums;
+    private int[] tree;
+
+    public NumArray02(int[] nums) {
+        int n = nums.length;
+        this.nums = nums;
+        tree = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            tree[i] += nums[i - 1];
+            int nxt = i + (i & -i); // 下一个关键区间的右端点
+            if (nxt <= n) {
+                tree[nxt] += tree[i];
+            }
+        }
+    }
+
+    public void update(int index, int val) {
+        int delta = val - nums[index];
+        nums[index] = val;
+        for (int i = index + 1; i < tree.length; i += i & -i) {
+            tree[i] += delta;
+        }
+    }
+
+    private int prefixSum(int i) {
+        int s = 0;
+        for (; i > 0; i &= i - 1) { // i -= i & -i 的另一种写法
+            s += tree[i];
+        }
+        return s;
+    }
+
+    public int sumRange(int left, int right) {
+        return prefixSum(right + 1) - prefixSum(left);
+    }
+}
