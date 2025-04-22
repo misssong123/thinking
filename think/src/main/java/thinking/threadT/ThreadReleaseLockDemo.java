9b2e9cf343a729c5319840ddd59b4274f82054ae
孟松 <mengsong@58.com>
2025-04-22 16:25:24 +0800

    线程构建样例

--- think/src/main/java/thinking/threadT/ThreadReleaseLockDemo.java
+++ think/src/main/java/thinking/threadT/ThreadReleaseLockDemo.java
@@ -11,7 +11,9 @@ public class ThreadReleaseLockDemo {
     public static ThreadReleaseLockDemo demo = new ThreadReleaseLockDemo();
     public static void main(String[] args) {
         //demo.releaseLock();
-        demo.noReleaseLock();
+        //demo.noReleaseLock();
+        //demo.joinReleaseLock();
+        demo.joinNoReleaseLock();
     }
     //WAITING状态/TIMED_WAITING状态释放锁
     public void releaseLock(){
@@ -48,6 +50,7 @@ public class ThreadReleaseLockDemo {
     //TIMED_WAITING状态不释放锁
     //调用 Thread.sleep(long millis)：不会释放锁。Thread.sleep() 是让当前线程暂停执行一段时间，但它不会释放线程持有的任何锁。
     //调用 LockSupport.parkNanos(long nanos) 等：不会释放锁。LockSupport 类提供的方法用于线程的阻塞和唤醒，调用 park 相关方法时，线程不会释放锁。
+    //join()方法：join() 仅释放被调用线程对象的锁，不影响其他锁。
     public void noReleaseLock(){
         Thread thread1 = new Thread(() -> {
             synchronized (LOCK){
@@ -76,6 +79,67 @@ public class ThreadReleaseLockDemo {
             }
         }, "thread2");
         thread1.start();
+        thread2.start();
+    }
+    //join() 仅释放被调用线程对象的锁，不影响其他锁。
+    public void joinReleaseLock(){
+        Thread thread1 = new Thread(() -> {
+            synchronized (this){
+                try{
+                    //获取到锁
+                    System.out.println(Thread.currentThread().getName()+":获取到锁");
+                    System.out.println(Thread.currentThread().getName()+":waiting....");
+                    TimeUnit.MILLISECONDS.sleep(2000);
+                    System.out.println(Thread.currentThread().getName()+":任务完成");
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }, "thread1");
+        Thread thread2 = new Thread(() -> {
+            synchronized (thread1){
+                try{
+                    //获取到锁
+                    System.out.println(Thread.currentThread().getName()+":获取到锁");
+                    thread1.start();
+                    TimeUnit.MILLISECONDS.sleep(2000);
+                    thread1.join();
+                    System.out.println(Thread.currentThread().getName()+":任务完成");
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }, "thread2");
+        thread2.start();
+    }
+    public void joinNoReleaseLock(){
+        Thread thread1 = new Thread(() -> {
+            synchronized (LOCK){
+                try{
+                    //获取到锁
+                    System.out.println(Thread.currentThread().getName()+":获取到锁");
+                    System.out.println(Thread.currentThread().getName()+":waiting....");
+                    TimeUnit.MILLISECONDS.sleep(2000);
+                    System.out.println(Thread.currentThread().getName()+":任务完成");
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }, "thread1");
+        Thread thread2 = new Thread(() -> {
+            synchronized (LOCK){
+                try{
+                    //获取到锁
+                    System.out.println(Thread.currentThread().getName()+":获取到锁");
+                    thread1.start();
+                    TimeUnit.MILLISECONDS.sleep(2000);
+                    thread1.join();
+                    System.out.println(Thread.currentThread().getName()+":任务完成");
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }, "thread2");
         thread2.start();
     }
 }
