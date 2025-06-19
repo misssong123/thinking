9f8a1ddf60dae48cff20c444e00b57b63775f5bb
孟松 <mengsong@58.com>
2025-06-19 16:34:52 +0800

    标签使用，不建议使用

---
+++ think/src/main/java/thinking/threadpooldemo/demo/demo/BreakLabelDemo.java
@@ -0,0 +1,19 @@
+package thinking.threadpooldemo.demo.demo;
+
+public class BreakLabelDemo {
+    public static void main(String[] args) {
+        retry:
+        for(int i = 0 ; i < 10 ; i++){
+            for(int j = 0 ; j < 1 ; j++){
+                if (i > 8){
+                    break retry;
+                }
+                if (i % 2 == 0){
+                    continue retry;
+                }
+                System.out.println("i:"+i+";j:"+j);
+            }
+        }
+        System.out.println("end");
+    }
+}
